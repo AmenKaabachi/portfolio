d11c23be3531938aa7ef40316492e7c093afc40b
Amen Kaabachi <amenallahkaabachi@gmail.com>
2024-12-29 14:50:19 +0100

    Your commit message

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const modeSwitch = document.querySelector('.mode-switch');
     
+    // Set default theme to light mode
+    document.body.classList.add('light-mode');
+
     modeSwitch.addEventListener('click', () => {
         document.body.classList.toggle('light-mode');
     });
 });
+
